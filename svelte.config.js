db3d2b95352c1810ca74a10034dd35a2f0ce4ec8
LuKa <kaj.lund@gmail.com>
2021-08-15 14:22:42 +0300

    Adds node build step

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,26 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    "extensions": [".svelte", ...mdsvexConfig.extensions],
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+// import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node';
 
-    kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+/** @type {import('@sveltejs/kit').Config} */
+
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	kit: {
+		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			env: {
+				host: 'HOST',
+				port: 'PORT'
+			}
+		})
 	},
 
-    preprocess: [mdsvex(mdsvexConfig)]
+	preprocess: [mdsvex(mdsvexConfig)]
 };
 
 export default config;
