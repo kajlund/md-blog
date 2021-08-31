4e5e301b408904ec19b3f512889fdedc6e2f43a6
LuKa <kaj.lund@gmail.com>
2021-08-31 18:28:10 +0300

    Adds feather icons lib and some fix to backToTop btn

--- src/lib/BackToTop.svelte
+++ src/lib/BackToTop.svelte
@@ -1,49 +1,51 @@
 <script>
-  export let showOnPx = 150;
-  let hidden = true;
+	import { ChevronUpIcon } from 'svelte-feather-icons';
 
-  function goTop() {
-    document.body.scrollIntoView();
-  }
+	export let showOnPx = 150;
+	let hidden = true;
 
-  function scrollContainer() {
-    return document.documentElement || document.body;
-  }
+	function goTop() {
+		document.body.scrollIntoView();
+	}
 
-  function handleOnScroll() {
-    if (!scrollContainer()) {
-      return;
-    }
+	function scrollContainer() {
+		return document.documentElement || document.body;
+	}
 
-    if (scrollContainer().scrollTop > showOnPx) {
-      hidden = false;
-    } else {
-      hidden = true;
-    }
-  }
+	function handleOnScroll() {
+		if (!scrollContainer()) {
+			return;
+		}
+
+		if (scrollContainer().scrollTop > showOnPx) {
+			hidden = false;
+		} else {
+			hidden = true;
+		}
+	}
 </script>
-
-<style>
-  .back-to-top {
-    padding: .4em .8rem;
-    border-radius: .5rem;
-    opacity: 1;
-    transition: opacity 0.5s, visibility 0.5s;
-    position: fixed;
-    z-index: 99;
-    right: 20px;
-    user-select: none;
-    bottom: 20px;
-    color: var(--grey-100);
-    background-color: var(--primary-500);
-  }
-
-  .back-to-top.hidden {
-    opacity: 0;
-    visibility: hidden;
-  }
-</style>
 
 <svelte:window on:scroll={handleOnScroll} />
 
-<div class="back-to-top" on:click={goTop} class:hidden>To Top</div>
+<div class="back-to-top" on:click={goTop} class:hidden><ChevronUpIcon size="26" /> Back To Top</div>
+
+<style>
+	.back-to-top {
+		padding: 0.4em 0.8rem;
+		border-radius: 0.5rem;
+		opacity: 1;
+		transition: opacity 0.5s, visibility 0.5s;
+		position: fixed;
+		z-index: 99;
+		right: 20px;
+		user-select: none;
+		bottom: 20px;
+		color: var(--grey-100);
+		background-color: var(--primary-500);
+	}
+
+	.back-to-top.hidden {
+		opacity: 0;
+		visibility: hidden;
+	}
+</style>
